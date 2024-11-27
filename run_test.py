ed076dd8dffce64ca2011f2dc9950ee06e3de6bd
shadowinder <gcc1117@gmail.com>
2024-11-27 18:42:49 +0800

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -13,4 +13,22 @@ fn = w.SaveImage()
 
 print("Saved",fn)
 
+'''
+import os
+
+from datetime import datetime
+from weather_landscape import WeatherLandscape
+
+
+
+
+
+w = WeatherLandscape()
+
+fn = w.SaveImage2()
+
+print("Saved",fn)
+with open("/root/weather_landscape/task_log.txt", "a") as log:
+    log.write("Task executed successfully at {}\n".format(datetime.now()))
+'''
     
